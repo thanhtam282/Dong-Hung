348c0c5a8ab65d322c84c9f5c6b4df7f99d65a9a
Pisces <>
2019-02-18 22:55:37 +0700

    update mixxin

--- _plugins/btnripple.js
+++ _plugins/btnripple.js
@@ -1,19 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-	// var buttonRipple = document.querySelector('.btn-ripple');
-	// buttonRipple.addEventListener('click', function (e) {
-	// 	let ripple = document.createElement('div');
-	// 	let x, y;
-	// 	var rect = e.target.getBoundingClientRect()
-	// 	// Lấy tọa độ của button
-	// 	x = e.clientX - rect.left;
-	// 	y = e.clientY - rect.top;
-	// 	ripple.classList.add('ripple');
-	// 	ripple.style.top = y + 'px';
-	// 	ripple.style.left = x + 'px';
-	// 	// console.log(x);
-	// 	buttonRipple.appendChild(ripple);
-	// 	window.setTimeout(function () {
-	// 		buttonRipple.removeChild(ripple)
-	// 	}, 1000);
-	// })
-})
+
+function btnRipple(btn) {
+	var buttonRipple = document.getElementsByClassName(btn);
+	Array.prototype.forEach.call(buttonRipple , function(element){
+		element.addEventListener('click', (e) => {
+			let ripple = document.createElement('div');
+			let x, y;
+			var rect = e.target.getBoundingClientRect()
+			// Lấy tọa độ của button
+			x = e.clientX - rect.left;
+			y = e.clientY - rect.top;
+			ripple.classList.add('ripple');
+			ripple.style.top = y + 'px';
+			ripple.style.left = x + 'px';
+			// console.log(x);
+			element.appendChild(ripple);
+			window.setTimeout(function () {
+				element.removeChild(ripple)
+			}, 1000);
+		});
+	})
+}
