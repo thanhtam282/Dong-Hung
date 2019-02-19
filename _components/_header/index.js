222ba9973e33843f6762fea709c87915515ed46e
Nguyen Sang <nnsang24@gmail.com>
2019-02-19 09:37:50 +0700

    fixed toggle

--- _components/_header/index.js
+++ _components/_header/index.js
@@ -1,24 +1,10 @@
 const header = {
-	headerClickMat: function(){
-		let hamburgerBtn = document.querySelector('.dh-header .header-hamburger');
-		hamburgerBtn.addEventListener('click', function (e) {
-			let ripple = document.createElement('div');
-			let x, y;
-			var rect = e.target.getBoundingClientRect()
-			// Lấy tọa độ của button
-			x = e.clientX - rect.left;
-			y = e.clientY - rect.top;
-			ripple.classList.add('ripple');
-			ripple.style.top = y + 'px';
-			ripple.style.left = x + 'px';
-			// console.log(x);
-			hamburgerBtn.appendChild(ripple);
-			window.setTimeout(function () {
-				hamburgerBtn.removeChild(ripple)
-			}, 1000);
+	headerToggle: function(){
+		$('.dh-header .header-hamburger').on('click', function(){
+			$('.dh-header').toggleClass('sidenav-open')
 		})
 	},
 	headerInit: function(){
-		header.headerClickMat();
+		header.headerToggle();
 	}
 }
