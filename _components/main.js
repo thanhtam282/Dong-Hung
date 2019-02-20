503e5e6da615b48f40e702abf5abcdc703cbbe78
Nguyen Sang <nnsang24@gmail.com>
2019-02-20 13:32:57 +0700

    done thu vien

--- _components/main.js
+++ _components/main.js
@@ -4,21 +4,16 @@ $(document).ready(() => {
 	btnRipple('btn-ripple');
 	nav.navInit();
 	objectFitImages();
-<<<<<<< HEAD
 	slider.aboutInit();
+	// Lĩnh vực
+	linhvuc_detail.linhvucDetailInit();
+	slider.about_slider_1();
+	slider.about_slider_2();
 });
 
 $(window).scroll(function () { 
 	if ($('.dh-nav-2').length) {
 		slider.checkMenuScroll();
 	}
-	
-=======
-
-	// Lĩnh vực
-	linhvuc_detail.linhvucDetailInit();
-	slider.about_slider_1();
-	slider.about_slider_2();
->>>>>>> 299b6f459004bfcae441902e6d2d18a9348bf2a2
 });
 
